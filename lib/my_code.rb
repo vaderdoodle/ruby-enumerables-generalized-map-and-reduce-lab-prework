77a07a4e1cd9b50a0cd2bb9fd9a17f820c42b104
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-22 18:23:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,25 +1,26 @@
 # Your Code Here
 def map(array)
-  result_array = []
+  new_array = []
   i = 0
 
    while i < array.length do
-    result_array[i] = yield(array[i]) #unique work
+    new_array[i] = yield(array[i])
     i += 1
   end
 
-   return result_array
+   return new_array
 end
 
-def reduce(array, starting_point = nil)
+def reduce(array, starting_point = 0)
   result = starting_point
   i = 0
 
-   while i < array.length do
+   while i < array.length 
+  do
     result = yield(result, array[i])
     i += 1
   end
 
-   return result #== 0 ? true : result
+   return result == 0 ? true : result
 
  end
