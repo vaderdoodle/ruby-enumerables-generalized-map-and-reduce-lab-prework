9cc5b3f36e8aab87a41f2ffcc2d5837a7e80c5ed
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-22 18:17:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,15 @@ def map(array)
    return result_array
 end
 
+def reduce(array, starting_point = 0)
+  result = starting_point
+  i = 0
+
+   while i < array.length do
+    result = yield(result, array[i])
+    i += 1
+  end
+
+   return result == 0 ? true : result
+
+ end
